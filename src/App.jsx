0edec10e847a4f9fba68fcc0fc43f73677195e6f
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-04-14 14:13:36 +0500

    remove things

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,17 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
-import styled from "styled-components";
 
-
-const Container = styled.div`
-  height: 100vh;
-  scroll-snap-type: y mandatory;
-  background: url("./img/bgN.jpg") right center no-repeat;
-  background-size: cover;
-  color: white;
-  scroll-behavior: smooth;
-  overflow-y: auto;
-  scrollbar-width: none;
-  &::-webkit-scrollbar {
-    display: none;
-  }  
-`
 
 function App() {
   return (
   <>
-    <Container>
+    <div className='container'>
       <Hero />
       <Who />
       <Works />
       <Contact />
-    </Container>
+    </div>
   </>
 
 )}
